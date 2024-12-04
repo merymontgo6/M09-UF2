a399eda58604363625c23ec8e77962762f54237f
Karolayn Muñoz <karolayn.mruiz@gmail.com>
2024-12-04 20:32:01 +0100

    comportament 1 fet

--- Fil.java
+++ Fil.java
@@ -1,20 +1,16 @@
 public class Fil extends Thread {
-    String name;
+    private String name;
+
     public Fil(String name) {
         this.name = name;
     }
 
-    //Comportament 1
     @Override
     public void run() {
-        try {
+      
             for (int i = 1; i <= 9; i++) {
                 System.out.println(name + " " + i);
-                Thread.sleep(100);
-            }
-        } catch (InterruptedException e) {
-            System.out.println("El fil ha estat interromput.");
-        }
+            }   
         System.out.println("Termina el fil " + name);
     }
 }
