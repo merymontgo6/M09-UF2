5bbb373d9e6ef7828ba4968912401cb91bfc9a3b
Karolayn Muñoz <karolayn.mruiz@gmail.com>
2024-12-04 20:17:19 +0100

    comportament 1 fet

--- Principal.java
+++ Principal.java
@@ -1,3 +1,9 @@
 public class Principal {
-    
+    public static void main(String[] args) {
+        Fil fil1 = new Fil("Juan");
+        Fil fil2 = new Fil("Pepe");
+        fil1.start();
+        fil2.start();
+        System.out.println("Fils iniciats des de Principal.");
+    }
 }
