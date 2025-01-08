b6c2158e618ddf3c16a5e865f3ed89f67422b7d2
Karolayn Muñoz <karolayn.mruiz@gmail.com>
2025-01-08 20:52:32 +0100

    03_Sleep_i_Coet fet a classe

---
+++ 03_Sleep_i_Coet/DormAleatori.java
@@ -0,0 +1,38 @@
+public class DormAleatori extends Thread {
+    
+    private final long creacio;
+
+    public DormAleatori(String nom) {
+        super(nom);
+        this.creacio = System.currentTimeMillis();
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++ ){
+            // Generar un interval aleatori
+            int intervalAleatori = (int) (Math.random() * 950) + 50; // intervalAleatori creat per chat
+            long tempsTotal = System.currentTimeMillis() - creacio; //temps de la creació del fil
+
+            System.out.println(getName() + "(" + i + ") a dormir " + intervalAleatori + "ms total " + tempsTotal);
+
+            try { // Dormir durant l'interval aleatori generat
+
+                Thread.sleep(intervalAleatori);
+            } catch (InterruptedException e) {}
+        }
+    }
+
+    public static void main (String[] args) throws InterruptedException{
+
+        String[] noms = {"Joan", "Pep"};
+        DormAleatori[] dorms = new DormAleatori[noms.length]; //array de fils per noms
+
+        for (int i = 0; i < noms.length; i++) {
+            dorms[i] = new DormAleatori(noms[i]); //es crea els fils
+            dorms[i].start();
+        }
+        Thread.sleep(50);
+        System.out.println("Fi de main --------------------");
+    }
+}
---
+++ 03_Sleep_i_Coet/Motor.java
@@ -0,0 +1,11 @@
+public class Motor extends Thread {
+    private static final float potenciaActual = 0;
+    private static final float potenciaObjectiu = 0;
+    private static final int maxTemps = 2;
+ //0 i 10 ambos incluidos
+    public void setPotencia(int p) {
+
+    }
+    
+    
+}
